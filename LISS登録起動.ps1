2b359d48969bf1e6735b1834fdf31d16f7c347c2
e1q08086 <e1q08086@yahoo.co.jp>
2019-11-20 00:11:47 +0900

    ps1

---
+++ LISS登録起動.ps1
@@ -0,0 +1,28 @@
+﻿$MyCMD = ".\logOutput.ps1 "
+Write-Host $MyCMD
+Invoke-Expression $MyCMD
+
+$MyCMD = "LogCreate " + $MyInvocation.MyCommand.Name + "_開始"
+Write-Host $MyCMD
+$Result = Invoke-Expression $MyCMD
+$Result
+
+
+#フォーマットチェック
+try{
+    cscript C:\script\wday.vbs >> $Result
+    Write-Host "vbs " $LASTEXITCODE
+    if($LASTEXITCODE -ne 0){
+        throw
+    }
+}catch{
+    Write-Output "異常" | Out-File $Result -Append -Encoding Default
+#    Write-Host "異常" >> $Result
+    Write-Error("エラー"+$_.Exception) >> $Result
+}finally{
+    $now = Get-Date
+    Write-Host "処理終了時間: " $now
+
+}
+
+
---
+++ logOutput.ps1
@@ -0,0 +1,46 @@
+﻿##########################################################################
+# ログ出力
+# 引数１：　
+#
+#
+##########################################################################
+function LogCreate(
+            $LogString
+            ){
+
+    # ログの出力先
+    $LogPath = "C:\script\log"
+
+    # ログファイル名
+    $LogName = "ExecutLog"
+
+    $Now = Get-Date
+
+    # Log 出力文字列に時刻を付加(YYYY/MM/DD HH:MM:SS.MMM $LogString)
+    $Log = $Now.ToString("yyyy/MM/dd HH:mm:ss.fff") + " "
+    $Log += $LogString
+
+    # ログファイル名が設定されていなかったらデフォルトのログファイル名をつける
+    if( $LogName -eq $null ){
+        $LogName = "LOG"
+    }
+
+    # ログファイル名(XXXX_YYYY-MM-DD.log)
+    $LogFile = $LogName + "_" +$Now.ToString("yyyy-MM-dd") + ".log"
+
+    # ログフォルダーがなかったら作成
+    if( -not (Test-Path $LogPath) ) {
+        New-Item $LogPath -Type Directory
+    }
+
+    # ログファイル名
+    $LogFileName = Join-Path $LogPath $LogFile
+
+    # ログ出力
+    Write-Output $Log | Out-File -FilePath $LogFileName -Encoding Default -append
+
+    # echo させるために出力したログを戻す
+    Return $LogFileName
+}
+
+#LogCreate $MyInvocation.MyCommand.Name
